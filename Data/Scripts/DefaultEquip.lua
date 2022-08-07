35c32f6e3528888dad3336f182b6596d4231d34c
standardcombo <standardcombo@gmail.com>
2022-08-07 01:40:55 -0700

    Added class testing for Sorcerer and Fighter.

--- Data/Scripts/DefaultEquip.lua
+++ Data/Scripts/DefaultEquip.lua
@@ -1,8 +1,36 @@
 
+local SORCERER = {
+	"Shift", "Teleport",
+	"1", "Tornado",
+	"2", "Animorph Potion",
+	"3", "Iceberg",
+	"4", "Pyroblast",
+}
 
+local FIGHTER = {
+	"Shift", "Roll",
+	"1", "Rock Strike",
+	"2", "Stone Wall",
+	"3", "Landslide",
+	"4", "Boulder Throw",
+}
+
+local SETUP = FIGHTER
+
+--[[
 Game.playerJoinedEvent:Connect(function(player)
     Events.Broadcast("Equipper_Equip", player, "Pyroblast", "1" )
     Events.Broadcast("Equipper_Equip", player, "Teleport", "2" )
     Events.Broadcast("Equipper_Equip", player, "Tornado", "3" )
     Events.Broadcast("Equipper_Equip", player, "Roll", "Shift" )
 end )
+]]
+
+Game.playerJoinedEvent:Connect(function(player)
+	for i = 1,#SETUP,2 do
+		local key = SETUP[i]
+		local ability = SETUP[i + 1]
+    	Events.Broadcast("Equipper_Equip", player, ability, key)
+    end
+end )
+
