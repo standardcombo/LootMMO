474be5128caba529a1023f92dcb2477623af686d
standardcombo <standardcombo@gmail.com>
2022-09-07 03:47:05 -0700

    Separate visibility and collision toggling now supported by QuestStateCompletedObject.lua

--- Data/Scripts/QuestStateCompletedObject.lua
+++ Data/Scripts/QuestStateCompletedObject.lua
@@ -1,15 +1,27 @@
 
 local QUEST_ID = script:GetCustomProperty("QuestID")
 local OBJECT = script:GetCustomProperty("Object"):WaitForObject()
+local SET_VISIBILITY = script:GetCustomProperty("SetVisibility")
+local SET_COLLISION = script:GetCustomProperty("SetCollision")
 
 local PLAYER = Game.GetLocalPlayer()
 
 
 function UpdateState()
 	if _G.QuestController.HasCompleted(PLAYER, QUEST_ID) then
-		OBJECT.visibility = Visibility.INHERIT
+		if SET_VISIBILITY then
+			OBJECT.visibility = Visibility.INHERIT
+		end
+		if SET_COLLISION then
+			OBJECT.collision = Collision.INHERIT
+		end
 	else
-		OBJECT.visibility = Visibility.FORCE_OFF
+		if SET_VISIBILITY then
+			OBJECT.visibility = Visibility.FORCE_OFF
+		end
+		if SET_COLLISION then
+			OBJECT.collision = Collision.FORCE_OFF
+		end
 	end
 end
 
