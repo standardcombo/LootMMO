920af8847d4dc8144bcaa5ec6936d27be1e43bcc
standardcombo <standardcombo@gmail.com>
2022-09-30 22:40:27 -0700

    Generalized the data and logic for granting quest rewards. Added inventory item for Coins.

---
+++ Data/Items/Coins.pbt
@@ -0,0 +1,14 @@
+Assets {
+  Id: 18068633884824446355
+  Name: "Coins"
+  PlatformAssetType: 33
+  SerializationVersion: 118
+  VirtualFolderPath: "Loot"
+  VirtualFolderPath: "Items"
+  VirtualFolderPath: "Materials"
+  ItemAsset {
+    MaximumStackCount: 9999999
+    CustomParameters {
+    }
+  }
+}
--- Data/Scripts/QuestController.lua
+++ Data/Scripts/QuestController.lua
@@ -485,18 +485,7 @@ function _GrantReward(player, rewardInstruction)
 		local materialDef = _G["Items.Materials"].GetDefinition(key)
 		amount = tonumber(amount)
 		if materialDef and amount then
-			print("Granting material: ".. key .." x".. amount)
-			Events.BroadcastToPlayer(player, "RewardToast", {
-				type = materialDef.rarity,
-				message = amount .." ".. materialDef.name,
-				icon = materialDef.icon,
-				flipH = materialDef.flipIconH,
-				flipV = materialDef.flipIconV,
-				sfx = materialDef.sfx,
-			})
-
-			-- TODO: Actually grant material
-
+			_GrantItem(player, materialDef, amount)
 		else
 			warn("Failed to grant material with instruction:".. rewardInstruction)
 		end
@@ -509,24 +498,43 @@ function _GrantReward(player, rewardInstruction)
 	else
 		local itemDef = _G["Items.More"].GetDefinition(instruction)
 		if itemDef then
-			print("Granting specific item: ".. instruction)
-			Events.BroadcastToPlayer(player, "RewardToast", {
-				type = itemDef.rarity,
-				message = itemDef.name,
-				icon = itemDef.icon,
-				flipH = itemDef.flipIconH,
-				flipV = itemDef.flipIconV,
-				sfx = itemDef.sfx,
-			})
-
-			if _G.FoundItemCinematic and itemDef.itemAsset then
-				_G.FoundItemCinematic.Show(player, itemDef.itemAsset)
-			end
-
-			-- TODO: Actually grant item
-
+			_GrantItem(player, itemDef, 1)
 		else
 			warn("Failed to grant specific item with instruction:".. rewardInstruction)
+		end
+	end
+end
+function _GrantItem(player, definition, amount)
+	--print("Granting item: ".. definition.id .." x".. amount)
+	-- Popup UI that shows the player what they got
+	local toastParams = {
+		type = definition.rarity,
+		message = definition.name,
+		icon = definition.icon,
+		flipH = definition.flipIconH,
+		flipV = definition.flipIconV,
+		sfx = definition.pickupSfx,
+	}
+	if amount > 1 then
+		toastParams.message = amount .."x ".. definition.name
+	end
+	Events.BroadcastToPlayer(player, "RewardToast", toastParams)
+
+	-- Play cinematic/animation of the player finding the item
+	-- Many rewards will not have this
+	if _G.FoundItemCinematic and definition.pickupAsset then
+		_G.FoundItemCinematic.Show(player, definition.pickupAsset)
+	end
+	
+	-- Actually grant the item (some reward elements may not have an item to grant)
+	if definition.itemAsset then
+		local char = _G["Character.EquipAPI"].GetCurrentCharacter(player)
+		if char then
+			local inv = char:GetComponent("Inventory"):GetInventory()
+			inv:AddItem(definition.itemAsset, {count = amount})
+		else
+			error("Tried to grant item ".. definition.id .." but player ".. 
+			      player.name .." didn't have a character")
 		end
 	end
 end
@@ -691,7 +699,9 @@ function OnChatMessage(player, params)
 		
 		for _,p in ipairs(Game.GetPlayers()) do
 			if string.lower(p.name) == string.lower(param2) then
-				ResetQuestsForPlayer(p)
+				Task.Spawn(function() -- This allows debugging with breakpoints
+					ResetQuestsForPlayer(p)
+				end)
 				break
 			end
 		end
