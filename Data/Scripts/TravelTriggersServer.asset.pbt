4e423de7848b84ca8b9f47eeb2bf34d5ce410850
standardcombo <standardcombo@gmail.com>
2022-10-07 22:48:51 -0700

    Added triggers to the outside doors of the tavern, so players can enter.

---
+++ Data/Scripts/TravelTriggersServer.asset.pbt
@@ -0,0 +1,24 @@
+Assets {
+  Id: 16171759585992088772
+  Name: "TravelTriggersServer"
+  PlatformAssetType: 3
+  TextAsset {
+    CustomParameters {
+      Overrides {
+        Name: "cs:SpawnUtils"
+        AssetReference {
+          Id: 11996838552400708578
+        }
+      }
+      Overrides {
+        Name: "cs:SpawnUtils:isrep"
+        Bool: false
+      }
+      Overrides {
+        Name: "cs:SpawnUtils:ml"
+        Bool: false
+      }
+    }
+  }
+  SerializationVersion: 118
+}
---
+++ Data/Scripts/TravelTriggersServer.lua
@@ -0,0 +1,52 @@
+--[[
+	TODO
+]]
+
+local SPAWN_UTILS = require( script:GetCustomProperty("SpawnUtils") )
+
+local TRIGGERS = script.parent:GetChildren()
+
+
+function TravelToGame(player, gameId)
+	player:TransferToGame(gameId)
+end
+
+
+function TravelToSpawnKey(player, spawnKey)
+	SPAWN_UTILS.SpawnPlayerAt(player, spawnKey)
+end
+
+
+function TravelToGameState(player, gameState)
+	_G.AppState.SetStateForPlayer(player, gameState)
+end
+
+
+function OnInteracted(trigger, player)
+    if _G.AppState then
+		local gameState = trigger:GetCustomProperty("GameState")
+		
+		gameState = tonumber(gameState)
+		
+		if gameState then
+			TravelToGameState(player, gameState)
+		end
+	end
+	
+	local spawnKey = trigger:GetCustomProperty("SpawnKey")
+	if spawnKey then
+		TravelToSpawnKey(player, spawnKey)
+	end
+
+	local gameId = trigger:GetCustomProperty("GameID")
+	if gameId then
+		TravelToGame(player, gameId)
+	end
+end
+
+for _,trigger in ipairs(TRIGGERS) do
+	if trigger.interactedEvent then
+		trigger.interactedEvent:Connect(OnInteracted)
+	end
+end
+
