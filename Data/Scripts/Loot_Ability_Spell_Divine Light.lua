ab9e8759c9c461a401609b90f951316fc3385510
blaking707 <46813060+blaking707@users.noreply.github.com>
2022-10-12 09:57:53 +1100

    Devine light status effect

--- Data/Scripts/Loot_Ability_Spell_Divine Light.lua
+++ Data/Scripts/Loot_Ability_Spell_Divine Light.lua
@@ -1,47 +1,52 @@
- 
 local ABILITY = script:GetCustomProperty('Ability'):WaitForObject()
 local ROOT = script:GetCustomProperty('Root'):WaitForObject()
-
+local API_SE = _G["StatusEffects.API"]
 local activateVFX = script:GetCustomProperty('HealerOrcDivineLightActiveBasic')
 local ImpulseAmount = 14000
 
 function AddImpulseToPlayer(ABILITY, player)
-    local directionVector = player:GetWorldPosition() - ABILITY.owner:GetWorldPosition()
-    directionVector = directionVector / directionVector.size
-    directionVector.z = 0.5
-    local impulseVector = directionVector * ImpulseAmount
+	local directionVector = player:GetWorldPosition() - ABILITY.owner:GetWorldPosition()
+	directionVector = directionVector / directionVector.size
+	directionVector.z = 0.5
+	local impulseVector = directionVector * ImpulseAmount
 
-    player:ResetVelocity()
-    player:AddImpulse(impulseVector)
+	player:ResetVelocity()
+	player:AddImpulse(impulseVector)
 end
 
 function Execute()
-    if ABILITY:GetCurrentPhase() == AbilityPhase.READY then
-        return
-    end
-    local mod = ROOT.serverUserData.calculateModifier()
-    local newObject =
-        World.SpawnAsset(
-        activateVFX,
-        {position = ABILITY.owner:GetWorldPosition(), networkContext = NetworkContextType.NETWORKED}
-    )
+	if ABILITY:GetCurrentPhase() == AbilityPhase.READY then
+		return
+	end
+	local mod = ROOT.serverUserData.calculateModifier()
+	local newObject =
+	World.SpawnAsset(
+		activateVFX,
+		{ position = ABILITY.owner:GetWorldPosition(), networkContext = NetworkContextType.NETWORKED }
+	)
 
-    local StunRadius = mod[ "Radius"]
-    local nearbyEnemies =
-        Game.FindPlayersInCylinder(ABILITY.owner:GetWorldPosition(), StunRadius, {ignoreTeams = ABILITY.owner.team})
+	local StunRadius = mod["Radius"]
+	local nearbyEnemies =
+	Game.FindPlayersInCylinder(ABILITY.owner:GetWorldPosition(), StunRadius, { ignoreTeams = ABILITY.owner.team })
 
-    ImpulseAmount = ImpulseAmount
+	ImpulseAmount = ImpulseAmount
 
-    warn('Add Status effect blind and speed')
-    local statusEffects = {}
-    local status = statusEffects.BLIND
-    local speedStatus = statusEffects.SPEED
-    local healAmmount = mod['Heal']
+	warn('Add Status effect blind and speed')
 
-    ABILITY.owner.hitPoints = CoreMath.Clamp(ABILITY.owner.hitPoints + healAmmount, ABILITY.owner.maxHitPoints)
 
-    for _, enemy in pairs(nearbyEnemies) do
-        AddImpulseToPlayer(ABILITY, enemy)
-    end
+	local statusEffects = {}
+	local status = statusEffects.BLIND
+	local speedStatus = statusEffects.SPEED
+	local healAmmount = mod['Heal']
+
+	API_SE.ApplyStatusEffect(ABILITY.owner, "SpeedBoost", {
+		source = ABILITY.owner,
+	})
+	ABILITY.owner.hitPoints = CoreMath.Clamp(ABILITY.owner.hitPoints + healAmmount, ABILITY.owner.maxHitPoints)
+
+	for _, enemy in pairs(nearbyEnemies) do
+		AddImpulseToPlayer(ABILITY, enemy)
+	end
 end
+
 ABILITY.executeEvent:Connect(Execute)
