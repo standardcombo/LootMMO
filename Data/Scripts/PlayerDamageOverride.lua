6b043d2a9537399a23aa743e0402f26d4aaa8818
luapi82 <6558978+luapi82@users.noreply.github.com>
2022-10-17 11:49:51 -0400

    Give 25% more damage to weapons when headshot tag is applied

--- Data/Scripts/PlayerDamageOverride.lua
+++ Data/Scripts/PlayerDamageOverride.lua
@@ -4,17 +4,23 @@ local function OnGoingToTakeDamage(attackData)
 	local item = attackData.item
 	local damage = attackData.damage
 	local dmgamout = damage.amount
+	local headshot = attackData.tags.Headshot
 	local source = attackData.source
 	if not Object.IsValid(source) then return end
 	if not source:IsA("Player") then return end
 	if dmgamout <= 0 then return end
-	
+a	
 	--overrideDamage
 	if not Object.IsValid(item) then return end
 	local char = EquipAPI.GetCurrentCharacter(source)
 	if char then
 		local stats = char:GetComponent("Stats")
-		dmgamout = stats:GetTempStat("AP")
+		--Check if Headshot tag is applied
+		if headshot then
+			dmgamout = stats:GetTempStat("AP") * 1.25
+		else
+			dmgamout = stats:GetTempStat("AP")
+		end
 	end
 	damage.amount = dmgamout
 end
