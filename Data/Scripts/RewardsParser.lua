712549213260d1f8f3150ada7bc25aeb6b22bdc0
standardcombo <standardcombo@gmail.com>
2022-10-17 19:06:57 -0700

    Documented RewardsParser.lua

--- Data/Scripts/RewardsParser.lua
+++ Data/Scripts/RewardsParser.lua
@@ -1,3 +1,55 @@
+--[[
+	Rewards Parser
+	v1.0 - 2022/10/20
+	by: standardcombo
+	
+	Parses reward instructions for the Loot Drop and Quest systems.
+	Uses a meta-scripting language that define complex reward sequences.
+	
+	Usage
+	=====
+	Added as a require() script to other scripts. Then call Parse()
+	
+	API
+	===
+	- Parse(player, dataTable, rowId)
+		Runs the instructions found in the given data table, at row "rowId".
+		Instructions should be in the column named "rewards".
+	
+	Meta-scripting
+	==============
+	Commands:
+	- Wait(float duration) A small pause in execution.
+	- Ref(string rewardRowId) Runs the instructions from another row of data.
+	- Loop(int count=string rewardRowId) Repeats another instruction multiple times.
+	- XP(int amount) Gives the player a specific amount of XP points (resource).
+	- Material(string itemId=int amount) Gives the player crafting materials.
+	- Item(string itemId) Gives the player a specific item.
+	- RandomItem(string paramName=int paramValue) Gives the player a random item.
+	- RNG[int minValue-int maxValue] Random number range. Supported in expressions.
+	- | 'or' operator, for multiple alternative parameters, chosen at random.
+	
+	Examples:
+	"Material(Coins=5)"
+		Gives the player 5 Coins.
+	
+	"Material(Coins=RNG[3-10])"
+		Gives the player a random amount of Coins, between 3 and 10.
+	
+	"Material(Coins=10), Wait(1), Item(MapPiece)"
+		Gives the player 10 Coins, waits 1 second, then gives them a MapPiece item.
+	
+	"Ref(RndMat), Wait(1), Ref(RndItem)"
+		Executes the instruction at row "RndMat", waits 1 second, then executes the
+		"RndItem" instruction.
+	
+	"Ref(RndMat | RndItem | CoinPile)"
+		Executes one instruction, from either "RndMat", "RndItem" or "CoinPile".
+		
+	"Loop(3=Bone1), Wait(0.5), Material(Coins=RNG[1-5])"
+		Executes the instruction at row "Bone1" three times, waits half a second,
+		then gives the player a random amount of coins, between 1 and 5.
+]]
 
 local API = {}
 
@@ -46,6 +98,7 @@ function _GrantReward(player, dataTable, rewardInstruction)
 	
 	-- Check all instructions
 	if instruction == "Wait" then
+		-- Add a small pause. Useful in-between rewards
 		local delay = tonumber(param)
 		if delay then
 			Task.Wait(delay)
@@ -58,6 +111,7 @@ function _GrantReward(player, dataTable, rewardInstruction)
 		_Parse(player, dataTable, param)
 		
 	elseif instruction == "Loop" then
+		-- Repeat a reward several times
 		local loopCount, ref = CoreString.Split(param, "=")
 		loopCount = _ParseAmount(loopCount)
 		for i = 1,loopCount do
@@ -76,6 +130,7 @@ function _GrantReward(player, dataTable, rewardInstruction)
 		_GrantItem(player, itemDef, resourceAmount)
 
 	elseif instruction == "Material" then
+		-- Give a crafting material
 		local key, amount = CoreString.Split(param, "=")
 		local materialDef = _G["Items.Materials"].GetDefinition(key)
 		amount = _ParseAmount(amount)
