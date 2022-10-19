0750e11433a74efbcec2b402a61f9766c564258e
CommanderFoo <pixeldepth@gmail.com>
2022-10-19 11:11:53 +0100

    Updated README on where to find the Fade Layer.

--- Data/Scripts/Main_README.lua
+++ Data/Scripts/Main_README.lua
@@ -9,7 +9,9 @@
 	==========
 	A top-level UI that obstructs the entire screen. This is what allows the game to
 	fade in from black when starting up.
-	To edit the scene, toggle visibility of the Fade Layer in the Hierarchy.
+	To edit the scene, toggle visibility of the Fade Layer. The Fade Layer can be found
+	in the "Top-Level UI" folder in the Hierarchy.
+
 	See the FadeLayer script for more info.
 	
 	
