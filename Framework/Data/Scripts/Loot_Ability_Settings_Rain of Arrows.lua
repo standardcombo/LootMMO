00f860a1576e866de48e00914b45e5e930edad19
standardcombo <standardcombo@gmail.com>
2022-12-24 20:12:17 -0800

    Swapped the Agility and Vitality usage in Rain of Arrows.

--- Framework/Data/Scripts/Loot_Ability_Settings_Rain of Arrows.lua
+++ Framework/Data/Scripts/Loot_Ability_Settings_Rain of Arrows.lua
@@ -7,9 +7,9 @@ local ABILITY_ID = 'Rain of Arrows'
 
 local modifiers = {}
 ModAPI.AddSkillPowerWithCrit(modifiers, 'Damage', 150, 1000)
-ModAPI.AddVitalityRNG(modifiers, 'CritChance')
+ModAPI.AddAgilityRNG(modifiers, 'CritChance')
 ModAPI.AddStarRatingScale(modifiers, 'CritMult', ABILITY_ID, 1.7, 0.1)
-ModAPI.AddAgilityScale(modifiers, 'Radius', 300, 1000)
+ModAPI.AddVitalityScale(modifiers, 'Radius', 300, 1000)
 ModAPI.AddStarRatingScale(modifiers, 'Cooldown', ABILITY_ID, 20, -0.75)
 
 
