bdfeded7f3d4f1807884b7853bee5abc325da646
luapi82 <6558978+luapi82@users.noreply.github.com>
2023-01-03 12:14:59 -0500

    Inventory Render / Crafting Scritps: Temporarily commented script section that shows upgraded NFT greatness in inventory / crafting inventory to prevent errors

--- Framework/Data/Scripts/Inventory_render_Crafting.lua
+++ Framework/Data/Scripts/Inventory_render_Crafting.lua
@@ -626,20 +626,22 @@ local function InventoryChanged(inv, slot)
 		if isBag then
 			if item:GetCustomProperty("IsBag") then
 				isBag.visibility = Visibility.INHERIT
-				for _, Collection in ipairs(COLLECTIONS) do
-					AsyncBC.GetTokensForPlayer(Game.GetLocalPlayer(), { contractAddress = Collection }, function(tokens)
-						for _, token in pairs(tokens) do
-							local parsedBag = LOOT_BAG_PARSER.Parse(token)
-							local items = parsedBag.items
-							local tokenString = CoreString.Join("|", token.contractAddress, token.tokenId)
-							for _, itemdata in pairs(items) do
-								if itemdata.name == item.name then
-									greatness = (nftSaves[tokenString] or {})[itemdata.name] or itemdata.greatness
-								end
-							end
-						end
-					end)
-				end
+				-- Uncomment the following section to show the greatness of the owned NFTs
+				-- Displays proper greatness for upgraded NFTs
+				-- for _, Collection in ipairs(COLLECTIONS) do
+				-- 	AsyncBC.GetTokensForPlayer(Game.GetLocalPlayer(), { contractAddress = Collection }, function(tokens)
+				-- 		for _, token in pairs(tokens) do
+				-- 			local parsedBag = LOOT_BAG_PARSER.Parse(token)
+				-- 			local items = parsedBag.items
+				-- 			local tokenString = CoreString.Join("|", token.contractAddress, token.tokenId)
+				-- 			for _, itemdata in pairs(items) do
+				-- 				if itemdata.name == item.name then
+				-- 					greatness = (nftSaves[tokenString] or {})[itemdata.name] or itemdata.greatness
+				-- 				end
+				-- 			end
+				-- 		end
+				-- 	end)
+				-- end
 			else
 				isBag.visibility = Visibility.FORCE_OFF
 			end
