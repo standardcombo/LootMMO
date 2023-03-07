68915f3d35eb2334f0495fafb5b4ab01b293b09b
standardcombo <standardcombo@gmail.com>
2023-03-06 23:51:53 -0800

    Completed Velwood door animations (Play/Stop with keys 9 and 0).

--- Framework/Data/Scripts/MagicDoorRevealVFX.lua
+++ Framework/Data/Scripts/MagicDoorRevealVFX.lua
@@ -11,6 +11,8 @@ local logoEmissivePeak
 local logoEmissive
 local pointLightsRoot
 local pointLightIntensity
+local pointLightY
+local pointLightZ
 local flareEmissive
 local spotLightIntensity
 
@@ -62,7 +64,126 @@ end
 
 
 function PlayAnimation()
-	SetEnabled() --TODO
+	-- Point Lights
+	AnimatePointLights()
+	
+	-- Logo
+	for _,glyph in ipairs(LOGO) do
+		AnimateLogoGlyph(glyph)
+		Task.Wait(0.1)--(0.3334)
+	end
+	
+	Task.Wait(2.1)
+	
+	-- Dust Motes
+	DUST:Play()
+	
+	-- Flare & Spotlight
+	SPOTLIGHT.visibility = Visibility.INHERIT
+	FLARE.visibility = Visibility.INHERIT
+	Task.Spawn(function()
+		local t = 0
+		local step = 0.0333333334
+		while true do
+			t = t + step
+			if t < 1.5 then
+				SPOTLIGHT.intensity = CoreMath.Lerp(SPOTLIGHT.intensity, spotLightIntensity, t)
+				local e = FLARE:GetSmartProperty("Emissive Brightness")
+				e = CoreMath.Lerp(e, flareEmissive, t)
+				FLARE:SetSmartProperty("Emissive Brightness", e)
+			else
+				SPOTLIGHT.intensity = spotLightIntensity
+				FLARE:SetSmartProperty("Emissive Brightness", flareEmissive)
+				break
+			end
+			Task.Wait(step)
+		end
+	end)
+end
+
+function AnimatePointLights()
+	pointLightsRoot.visibility = Visibility.INHERIT
+	for _,p in ipairs(POINT_LIGHTS) do
+		p.intensity = 0
+		p:SetPosition(Vector3.ZERO)
+	end
+	
+	Task.Spawn(function()
+		Task.Wait(1)
+		
+		local step = 0.0333333334
+		-- Move vertical
+		local t = 0		
+		while true do
+			t = t + step
+			local pos = Vector3.New(0, 0, CoreMath.Lerp(0, pointLightZ, t))
+			for i,p in ipairs(POINT_LIGHTS) do
+				p.intensity = CoreMath.Lerp(p.intensity, pointLightIntensity, t * 5)
+				if i >= 3 then
+					pos.z = -pos.z
+				end
+				p:SetPosition(pos)
+			end
+			if t >= 1 then break end
+			Task.Wait(step)
+		end
+		-- Move horizontal
+		t = 0
+		while true do
+			t = t + step
+			local pos = Vector3.New(0, CoreMath.Lerp(0, pointLightY, t), pointLightZ)
+			for i,p in ipairs(POINT_LIGHTS) do
+				if i == 2 or i == 4 then
+					pos.y = -pos.y
+				end
+				if i >= 3 then
+					pos.z = -pos.z
+				end
+				p:SetPosition(pos)
+			end
+			if t >= 1 then break end
+			Task.Wait(step)
+		end
+		-- Fade out
+		t = 0
+		while true do
+			t = t + step
+			for i,p in ipairs(POINT_LIGHTS) do
+				p.intensity = CoreMath.Lerp(p.intensity, 0, t)
+			end
+			if t >= 2 then break end
+			Task.Wait(step)
+		end
+		pointLightsRoot.visibility = Visibility.FORCE_OFF
+	end)
+end
+
+function AnimateLogoGlyph(glyph)
+	Task.Spawn(function()
+		local t = 0
+		local step = 0.0333333334
+		local _lerp = step * 10
+		while true do
+			t = t + step
+			if t < 1.5 then
+				local c = glyph:GetSmartProperty("Color")
+				local e = glyph:GetSmartProperty("Emissive Boost")
+				c = Color.Lerp(c, logoColorOn, _lerp)
+				if t < 0.2 then
+					e = CoreMath.Lerp(e, logoEmissivePeak, _lerp)
+				else
+					e = CoreMath.Lerp(e, logoEmissive, _lerp)
+				end
+				glyph:SetSmartProperty("Color", c)
+				glyph:SetSmartProperty("Emissive Boost", e)
+			else
+				glyph:SetSmartProperty("Color", logoColorOn)
+				glyph:SetSmartProperty("Emissive Boost", logoEmissive)
+				break
+			end
+			Task.Wait(step)
+		end
+	end)
 end
 
 
@@ -78,6 +199,9 @@ function InitPointLights()
 	pointLightsRoot = POINT_LIGHTS
 	POINT_LIGHTS = POINT_LIGHTS:GetChildren()
 	pointLightIntensity = POINT_LIGHTS[1].intensity
+	local pos = POINT_LIGHTS[1]:GetPosition()
+	pointLightY = pos.y
+	pointLightZ = pos.z
 end
 
 function InitOtherFX()
