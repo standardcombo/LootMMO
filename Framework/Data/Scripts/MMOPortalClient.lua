87c13a1d230a477fe53f13010c05458d0e85a524
Morituri-SK <0ndrej415@gmail.com>
2023-03-09 11:20:14 +0100

    unfreeze camera after portalling back from adventure

--- Framework/Data/Scripts/MMOPortalClient.lua
+++ Framework/Data/Scripts/MMOPortalClient.lua
@@ -92,6 +92,10 @@ function OnInteracted(trigger, player)
 	if player == Game.GetLocalPlayer() then
 		Events.BroadcastToServer(TRANSFER_EVENT_ID, ROOT.id)
 		Events.Broadcast("FreezeCamera")
+		--TODO unfreeze camera? Do not freeze camera?
+		Task.Spawn(function()
+			Events.Broadcast("UnfreezeCamera")
+		end,.1)
 	end
 end
 
