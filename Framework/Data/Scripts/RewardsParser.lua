7b748793c47f45d29ee0e71167f36a9c631aae51
standardcombo <standardcombo@gmail.com>
2023-03-30 18:49:49 -0700

    RewardsParser: Added support for PlayerLevel, full math expressions and even nested RNG calls.

--- Framework/Data/Scripts/RewardsParser.lua
+++ Framework/Data/Scripts/RewardsParser.lua
@@ -1,10 +1,11 @@
 --[[
 	Rewards Parser
-	v1.0.1 - 2022/10/25
+	v1.1.0 - 2023/3/30
 	by: standardcombo
 	
 	Parses reward instructions for Loot Drops, Quests, or any other system.
 	Uses a meta-scripting language that define complex reward sequences.
+	Supports full math expressions.
 	
 	
 	Usage
@@ -59,6 +60,9 @@
 	"Loop(3=Bone1), Wait(0.5), Material(Coins=RNG[1-5])"
 		Executes the instruction at row "Bone1" three times, waits half a second,
 		then gives the player a random amount of coins, between 1 and 5.
+
+	"Material(Coins=PlayerLevel*10)"
+		Gives the player a number of Coins equal to their level multiplied by 10.
 ]]
 
 local API = {}
@@ -89,11 +93,30 @@ function _Parse(player, dataTable, rowId)
 	end
 end
 
+local function GetPlayerLevel(player)
+	if _G["Character.EquipAPI"] then
+		local char = _G["Character.EquipAPI"].GetCurrentCharacter(player)
+		if char then
+			return char:GetComponent("Level").level
+		end
+	end
+	return player:GetResource("Level")
+end
+
 function _GrantReward(player, dataTable, rewardInstruction)
-	local instruction, param = CoreString.Split(rewardInstruction, {
-		delimiters = {"(", ")"}, 
-		removeEmptyResults = true
-	})
+	-- Replace `PlayerLevel` variable with player's actual level
+	local playerLevel = GetPlayerLevel(player)
+	rewardInstruction = rewardInstruction:gsub("PlayerLevel", tostring(playerLevel))
+	
+	-- Normalize case for RNG macro
+	rewardInstruction = rewardInstruction:gsub("rng%[", "RNG[")
+	rewardInstruction = rewardInstruction:gsub("Rng%[", "RNG[")
+	
+	local instruction, param = rewardInstruction:match("^(.-)%((.+)%)$")
+	
+	if not instruction or not param then
+		error("Malformed reward instruction '"..rewardInstruction.."'")
+	end
 	
 	-- Case of multiple parameters separated by '|', choose one at random
 	local alternativeParams = { CoreString.Split(param, "|") }
@@ -188,26 +211,6 @@ function _GrantReward(player, dataTable, rewardInstruction)
 	end
 end
 
-function _ParseNumber(value)
-	if not value then
-		return nil
-	end
-	local num = tonumber(value)
-	if num then
-		return num
-	end
-	local min, max = CoreString.Split(value, {
-		delimiters = {"RNG[", "rng[", "Rng[", "-", "]"}, 
-		removeEmptyResults = true
-	})
-	min = tonumber(min)
-	max = tonumber(max)
-	if min and max then
-		return math.random(min, max)
-	end
-	return nil
-end
-
 
 function SplitCommaSeparatedData(data)
 	-- Split with comma
@@ -222,6 +225,128 @@ function SplitCommaSeparatedData(data)
 		results[i] = CoreString.Trim(entry)
 	end
 	return results
+end
+
+
+function tokenize(expr)
+	local tokens = {}
+	expr = expr:gsub("%s+", "") -- remove whitespace
+	local i = 1
+	while i <= #expr do
+		local c = expr:sub(i, i)
+		if c:match("%d") or c == "." then -- parse number
+			local j = i + 1
+			while j <= #expr and (expr:sub(j, j):match("%d") or expr:sub(j, j) == ".") do
+				j = j + 1
+			end
+			tokens[#tokens+1] = expr:sub(i, j-1)
+			i = j
+		elseif c:match("[%+%-*/%(%)%%^]") then -- parse operator
+			tokens[#tokens+1] = c
+			i = i + 1
+		else -- unrecognized character
+			error("Invalid token '"..c.."' in expression: " ..expr)
+		end
+	end
+	return tokens
+end
+
+
+function _ParseNumber(str)
+	-- Define operator precedence levels (reversed order)
+	local precedence = {
+		"+",
+		"-",
+		"*",
+		"/",
+	}
+	
+	-- Inner function that evaluates a sub-expressions. Allows recursion
+	local function evaluate(expr)
+		--print("evaluate() "..expr)
+		-- Evaluate expressions inside parentheses first
+		expr = expr:gsub("^(.-)%((.+)%)(.-)$", function(a, b, c)
+			--print(a)
+			--print(b)
+			--print(c)
+			if b then
+				return a..tostring(evaluate(b))..c
+			end
+			return expr
+		end)
+		-- Replace RNG[a-b] constructs with random numbers
+		local function replace_rng(pre, a, b, post)
+			--print(pre)
+			--print(a)
+			--print(b)
+			--print(post)
+			-- parse the expressions for a and b
+			local a_val = _ParseNumber(a)
+			if not a_val then
+				error("Failed to parse expression: " .. a)
+			end
+			local b_val = _ParseNumber(b)
+			if not b_val then
+				error("Failed to parse expression: " .. b)
+			end
+			-- compute the range of possible values
+			local min_val = CoreMath.Round(math.min(a_val, b_val))
+			local max_val = CoreMath.Round(math.max(a_val, b_val))
+			-- generate and return a random number in the range
+			return pre.. tostring(math.random(min_val, max_val)) ..post
+		end
+		-- Start with the inner-most RNG[] block. Pad with spaces to ease parsing
+		local paddedExpr = ' '..expr..' '
+		local rngExpr = paddedExpr:gsub("^(.+)RNG%[(.-)%-(.-)%](.+)$", replace_rng)
+		if rngExpr ~= paddedExpr then
+			--print("rngExpr: "..rngExpr)
+			return evaluate(rngExpr)
+		end
+		
+		-- Most basic case, the expression is simply a number
+		local number = tonumber(expr)
+		if number then
+			return number
+		end
+		
+		-- Split the expression into tokens
+		local tokens = tokenize(expr)
+		
+		-- Evaluate the tokens in order of operator precedence
+		for i = 1, #precedence do
+			local op = precedence[i]
+			for j = 2, #tokens - 1 do
+				if tokens[j] == op then
+					-- Evaluate the expression to the left of the operator
+					local left_expr = table.concat(tokens, " ", 1, j - 1)
+					local left_val = evaluate(left_expr)
+					
+					-- Evaluate the expression to the right of the operator
+					local right_expr = table.concat(tokens, " ", j + 1)
+					local right_val = evaluate(right_expr)
+					
+					-- Apply the operator to the two values
+					local result
+					if op == "+" then
+						return left_val + right_val
+					elseif op == "-" then
+						return left_val - right_val
+					elseif op == "*" then
+						return left_val * right_val
+					elseif op == "/" then
+						return left_val / right_val
+					end
+					error("Unimplemented operator '"..op.."' in reward string '"..str.."'")
+					return 0
+				end
+			end
+		end
+		error("Could not parse reward string '"..str.."', check syntax")
+		return 0
+	end
+
+	-- Evaluate the expression and return the result
+	return evaluate(str)
 end
 
 
